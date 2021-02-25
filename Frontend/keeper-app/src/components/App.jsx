a99c6b0f2aa81ecf18f9647acbd1cb1b0e89a3d4
Pushpendra Vishwakarma <pushpendra.hpx2001@gmail.com>
2021-02-25 20:34:53 +0530

    removed

--- Frontend/keeper-app/src/components/App.jsx
+++ Frontend/keeper-app/src/components/App.jsx
@@ -17,9 +17,9 @@ function App() {
   function deleteNote(id){
  
     setNotes(prevNotes =>{
-return prevNotes.filter( (noteItem,index) =>{
- return index !==id;
-});
+    return prevNotes.filter( (noteItem,index) =>{
+    return index !==id;
+    });
 
     });
 
