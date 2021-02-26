f8396380cd1ac0cc5af782d31f66b37fceabcb49
Pushpendra Vishwakarma <pushpendra.hpx2001@gmail.com>
2021-02-26 19:58:08 +0530

    Main Template

---
+++ Frontend/keeper-app/src/Pages/Profile.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import default_profile from './../assets/some_icon1.svg';
+import './profile.css';
+
+export default class Profile extends Component{
+    
+    constructor(props){
+        super()
+
+
+        this.state = {
+            username:props.match.params.id,
+            name:'',
+            obj:{}
+        }
+    }
+    makeApiRequest(data){
+
+    }
+
+
+    componentDidMount(){
+        fetch('https://api.github.com/users/'+this.state.username)
+        .then(response=>{
+            response.json().then(data=>{
+                this.setState(init=>{
+                    console.log(data)
+                    return {...init, name:data.name,obj:data}
+                })
+            })
+        }).catch(e=>{
+            console.log('Failed Request')
+        })    
+    }
+
+
+
+
+    render(){
+        return <div className="profileBody">
+            <div class="side_wrapper">
+  <section class="about-dev">
+    <header class="profile-card_header">
+      <div class="profile-card_header-container">
+        <div class="profile-card_header-imgbox">
+          <img src="http://placekitten.com/600/900" alt="Mewy Pawpins" />
+        </div>
+        <h1> {this.state.name} <span> Username : {this.state.username} </span></h1>
+      </div>
+    </header>
+    <div class="profile-card_about">
+      <h2>All About Mewy</h2>
+      <p> { this.state.obj.bio } </p>
+      <footer class="profile-card_footer">
+        <div class="social-row">
+          <div class="heart-icon" title="No Health Issues">
+            <img src={default_profile} />
+          </div>
+
+        </div>
+        <p><a class="back-to-profile" href="#">Full Adoption Profile</a></p>
+      </footer>
+    </div>
+  </section>
+</div>
+        </div>
+    }
+}
--- Frontend/keeper-app/src/components/App.jsx
+++ Frontend/keeper-app/src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Blogs from './../Pages/Blogs';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from "../Pages/Dashboard";
+import Profile from "../Pages/Profile";
+import PageNotFound from './../Pages/PageNotFound';
+
 
 function App() {
   return <BrowserRouter>
     <Switch>
       <Route exact={true} path="/" component={Dashboard} />
       <Route path="/blogs" component={Blogs} />
+      <Route path="/profile/:id" component={Profile} />
+      <Route path="**" component={PageNotFound} />
     </Switch>
   </BrowserRouter>
 }
