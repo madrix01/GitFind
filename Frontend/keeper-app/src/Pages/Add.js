8099a46d559f36ae573202f2afb60e3be5ce85f2
Harshit Chauhan <u19ec002@eced.svnit.ac.in>
2021-02-28 00:04:32 +0530

    modified add route

--- Frontend/keeper-app/src/Pages/Add.js
+++ Frontend/keeper-app/src/Pages/Add.js
@@ -1,13 +1,78 @@
-import React, { Component } from 'react';
-export default class Add extends Component{
-    constructor(props){
-        super(props)
-        
+import React,{ useState, useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 
-    }
-    render(){
-        return <div>
-            Add Now
-        </div>
-    }
+const filter = createFilterOptions();
+
+export default function FreeSoloCreateOption() {
+  const [value, setValue] = React.useState(null);
+  const [users,setUsers]=useState(null);
+  useEffect( async() =>{
+   const response= await fetch('https://api.github.com/users/Harshit2929/repos');
+   const data= await response.json();
+   //console.log(data);
+   setUsers(data);
+  },[]);
+  
+
+  return (
+    <Autocomplete
+      value={value}
+      onChange={(event, newValue) => {
+        if (typeof newValue === 'string') {
+          setValue({
+            name: newValue,
+          });
+        } else if (newValue && newValue.inputValue) {
+          // Create a new value from the user input
+          setValue({
+            name: newValue.inputValue,
+          });
+        } else {
+          setValue(newValue);
+        }
+      }}
+      filterOptions={(options, params) => {
+        const filtered = filter(options, params);
+
+        // Suggest the creation of a new value
+        if (params.inputValue !== '') {
+          filtered.push({
+            inputValue: params.inputValue,
+            name: `Add "${params.inputValue}"`,
+          });
+        }
+
+        return filtered;
+      }}
+      selectOnFocus
+      clearOnBlur
+      handleHomeEndKeys
+      id="free-solo-with-text-demo"
+      options={users}
+      getOptionLabel={(option) => {
+        // Value selected with enter, right from the input
+        if (typeof option === 'string') {
+          return option;
+        }
+        // Add "xxx" option created dynamically
+        if (option.inputValue) {
+          return option.inputValue;
+        }
+        // Regular option
+        return option.name;
+      }}
+      renderOption={(option) => option.name}
+      style={{ margin: 'auto',
+  width: '50%',
+   padding: '10px'}}
+      freeSolo
+      renderInput={(params) => (
+        <TextField {...params} label="Select repos you want to add" variant="outlined" />
+      )}
+    />
+  );
 }
+
+
+
